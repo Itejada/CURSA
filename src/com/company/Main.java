9f66dca048bedcaf0758a3360e4a6f19fa1a0154
Itejada <ismael.elpuig@gmail.com>
2018-10-19 18:40:25 +0200

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,42 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
+
+/*
+ Caracteristica 1: preparar cursa
+ Escenari: Triar participants
+ Given pregunta cuants participants  hi ha
+ ehrn infiquem  el numero  de partc
+ then crear pilots i cotxes
+
+ Escenari: indicar el circuit on es fa la cursa
+ quan indiquem el nom
+ aleeshores guardem el nom de la cursa
+
+ Caracteristica 2: indicar cursa
+ Escenari : començar compte enrera
+ Given pilots i cotxes entrats
+ When premem intro final
+ then començar compre enrere i comença la cursa
+
+ Scenario: cursa acabada
+ Given que el pilots corren
+ When acabin
+ Then mostra classificació
+
+
+  Class:
+  Vheicle --> Cotxe --> F1/4x4/etc
+  Vheicle --> moto --> Mgp/M3
+  Vheicle --> bicicletas  --> Mgp/M3
+  *********************
+  Persona --> Pilot
+  **************
+  Circuit
+
+*/
